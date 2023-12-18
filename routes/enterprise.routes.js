ed2e2857ef426aab32c23c65a25ad79c29e21686
Surajit80kotai <surajit80kotai@gmail.com>
2023-12-18 13:05:29 +0530

    update

--- routes/enterprise.routes.js
+++ routes/enterprise.routes.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const EnterpriseController = require('../controllers/Enterprise/enterpriseController');
-const { adminEmptyCheck, userEmptyCheck } = require('../middleware/enterprise/enterprise.middleware');
-const { duplicateUserCheck } = require('../middleware/auth.validation');
 const router = express.Router();
 
 
 router.get('/get/enterprise/list', EnterpriseController.list).name = "EnterpriseList";
-// enterprise admin
-router.post('/add/enterprise', [adminEmptyCheck, duplicateUserCheck], EnterpriseController.addEnterprise);
-// enterprise user
-router.post('/add/enterprise/user', [userEmptyCheck, duplicateUserCheck], EnterpriseController.addEnterpriseUser);
+
 // set password
 router.get('/set/new/password/:hashValue', EnterpriseController.SetNewPasswordView);
 router.post('/set/new/password/:hashValue', EnterpriseController.SetNewPassword);
--- routes/super.admin.routes.js
+++ routes/super.admin.routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const ReportController = require('../controllers/SuAdmin/report.controller');
+const UserCreateController = require('../controllers/SuAdmin/userCreate.controller');
 const verifyToken = require('../middleware/authentication.middleware');
 const routeAccessMiddleware = require('../middleware/access.middleware');
+const { adminEmptyCheck, userEmptyCheck } = require('../middleware/enterprise/enterprise.middleware');
+const { duplicateUserCheck } = require('../middleware/auth.validation');
 const router = express.Router();
 
 router.get('/get/all/data', verifyToken, routeAccessMiddleware(), ReportController.AllDataLog);
+// enterprise admin
+router.post('/add/enterprise', [verifyToken, routeAccessMiddleware(), adminEmptyCheck, duplicateUserCheck], UserCreateController.addEnterprise);
+// enterprise user
+router.post('/add/enterprise/user', [verifyToken, routeAccessMiddleware(), userEmptyCheck, duplicateUserCheck], UserCreateController.addEnterpriseUser);
 
 
 // router.get('/hi', routeAccessMiddleware(), UserController.index);

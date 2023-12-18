b1a1d5eb7cf3ce8f283942688a9add168158eaa4
Surajit80kotai <surajit80kotai@gmail.com>
2023-12-18 14:38:16 +0530

    update

--- routes/enterprise.routes.js
+++ routes/enterprise.routes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const EnterpriseController = require('../controllers/Enterprise/enterpriseController');
 const router = express.Router();
 
-
-router.get('/get/enterprise/list', EnterpriseController.list).name = "EnterpriseList";
-
 // set password
 router.get('/set/new/password/:hashValue', EnterpriseController.SetNewPasswordView);
 router.post('/set/new/password/:hashValue', EnterpriseController.SetNewPassword);
--- routes/super.admin.routes.js
+++ routes/super.admin.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ReportController = require('../controllers/SuAdmin/report.controller');
 const UserCreateController = require('../controllers/SuAdmin/userCreate.controller');
+const EnterpriseController = require('../controllers/Enterprise/enterpriseController');
 const verifyToken = require('../middleware/authentication.middleware');
 const routeAccessMiddleware = require('../middleware/access.middleware');
 const { adminEmptyCheck, userEmptyCheck } = require('../middleware/enterprise/enterprise.middleware');
@@ -8,6 +9,8 @@ const { emptyUserCheck, duplicateUserCheck, duplicateEnterpriseCheck } = require
 const router = express.Router();
 
 router.get('/get/all/data', verifyToken, routeAccessMiddleware(), ReportController.AllDataLog);
+// enterprise list
+router.get('/get/enterprise/list', EnterpriseController.list).name = "EnterpriseList";
 // add enterprise admin
 router.post('/add/enterprise', [verifyToken, routeAccessMiddleware(), adminEmptyCheck, duplicateEnterpriseCheck], UserCreateController.addEnterprise);
 // add enterprise user

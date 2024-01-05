b317af91b2724a08913fc4bf10fd86a040260487
Satyajit <satyajit@kotaielectronics.com>
2024-01-05 19:52:03 +0530

    Optimizer Settings Value SET & RESET API created

--- controllers/Hardware/HardwareController.js
+++ controllers/Hardware/HardwareController.js
@@ -1,9 +1,14 @@
 const GatewayLogModel = require('../../models/GatewayLog.model');
 const OptimizerLogModel = require('../../models/OptimizerLog.model');
-const GatewayModel = require('../../models/gateway.model');
-const OptimizerModel = require('../../models/optimizer.model');
 const OptimizerDefaultSettingValueModel = require('../../models/OptimizerDefaultSettingValue.model');
 const OptimizerSettingValueModel = require('../../models/OptimizerSettingValue.model');
+const OptimizerModel = require('../../models/optimizer.model');
+const GatewayModel = require('../../models/gateway.model');
+const LocationModel = require('../../models/enterprise_state_location.model');
+const StateModel = require('../../models/enterprise_state.model');
+const EnterpriseModel = require('../../models/enterprise.model');
+
+
 
 exports.Config = async (req, res) => {
     // NGCS2023011003
@@ -218,31 +223,113 @@ exports.OptimizerDefaultSettingValue = async (req, res) => {
     }
 };
 
+
+// SetOptimizerSettingValue 
 exports.SetOptimizerSettingValue = async (req, res) => {
     try {
-        const {
-            optimizerID,
-            powerOnObservation,
-            maxCompressorTurnoffCountPerHour,
-            optimizationTime,
-            steadyStateRoomTemperatureTolerance,
-            steadyStateCoilTemperatureTolerance,
-            steadyStateSamplingDuration,
-            minAirConditionerOffDuration,
-            airConditionerOffDeclarationMinPeriod,
-            maxObservationTime,
-            thermoStateTimeIncrease,
-            thermoStateInterval
-        } = req.body;
+        const data = {
+            powerOnObservation: req.body.powerOnObservation,
+            maxCompressorTurnoffCountPerHour: req.body.maxCompressorTurnoffCountPerHour,
+            optimizationTime: req.body.optimizationTime,
+            steadyStateRoomTemperatureTolerance: req.body.steadyStateRoomTemperatureTolerance,
+            steadyStateCoilTemperatureTolerance: req.body.steadyStateCoilTemperatureTolerance,
+            steadyStateSamplingDuration: req.body.steadyStateSamplingDuration,
+            minAirConditionerOffDuration: req.body.minAirConditionerOffDuration,
+            airConditionerOffDeclarationMinPeriod: req.body.airConditionerOffDeclarationMinPeriod,
+            maxObservationTime: req.body.maxObservationTime,
+            thermoStateTimeIncrease: req.body.thermoStateTimeIncrease,
+            thermoStateInterval: req.body.thermoStateInterval
+        };
 
-        const result = await OptimizerSettingValueModel.findOneAndUpdate(
-            { optimizerID },
-            { $set: { ...req.body } },
-            { new: true, upsert: true }
-        );
+        // reset particular optimizer
+        if (req.body.group === 'optimizer') {
+            console.log(`Setting value particular optimizer ${req.body.id}`);
+            const optimizerIDS = [req.body.id]
+            result = await UpdateSettings(optimizerIDS, data);
+        }
+
+        // reset all optimizer assign with the gateway => optimizers
+        if (req.body.group == 'gateway') {
+            console.log(`Setting value gateway => optimizers`);
+            const gateway_id = req.body.id;
+            const allOptimizer = await OptimizerModel.find({ GatewayId: gateway_id });
+            const optimizerIDS = await Promise.all(allOptimizer.map(async (item) => {
+                return item._id;
+            }));
+
+            result = await UpdateSettings(optimizerIDS, data);
+        }
+
+        // reset all optimizer assign with the location => gateways => optimizers
+        if (req.body.group == 'location') {
+            console.log(`Setting value location => gateways => optimizers`);
+            const location_id = req.body.id;
+            const Location = await LocationModel.findOne({ _id: location_id });
+            const allGateway = await GatewayModel.find({ EnterpriseInfo: Location._id });
+
+            const optimizerIDS = await Promise.all(allGateway.map(async (gateway) => {
+                const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                return allOptimizer.map((item) => item._id);
+            }));
+
+            result = await UpdateSettings(optimizerIDS.flat(), data);
+        }
+
+        // reset all optimizer assign with the state => locations => gateways => optimizers
+        if (req.body.group == 'state') {
+            console.log(`Setting value state => locations => gateways => optimizers`);
+            const state_id = req.body.id;
+            const State = await StateModel.findOne({ _id: state_id });
+            const allLocation = await LocationModel.find({ State_ID: State.State_ID });
+            const optimizerIDS = await Promise.all(allLocation.map(async (location) => {
+                const allGateway = await GatewayModel.find({ EnterpriseInfo: location._id });
+
+                // Using Promise.all to wait for all OptimizerModel.find() queries to complete
+                const optimizerPromises = allGateway.map(async (gateway) => {
+                    const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                    return allOptimizer.map((item) => item._id);
+                });
+
+                return Promise.all(optimizerPromises);
+            }));
+            const flattenedOptimizerIDs = optimizerIDS.flat();
+            // console.log(flattenedOptimizerIDs.flat());
+            result = await UpdateSettings(flattenedOptimizerIDs.flat(), data);
+        }
+
+        // reset all optimizer assign with the enterprise => states => locations => gateways => optimizers
+        if (req.body.group === 'enterprise') {
+            console.log(`Setting value enterprise => states => locations => gateways => optimizers`);
+
+            const enterprise_id = req.body.id;
+            const Enterprise = await EnterpriseModel.findOne({ _id: enterprise_id });
+            const allState = await StateModel.find({ Enterprise_ID: Enterprise._id });
+
+            const optimizerIDS = await Promise.all(allState.map(async (state) => {
+                const allLocation = await LocationModel.find({ State_ID: state.State_ID });
+
+                // Using Promise.all to wait for all OptimizerModel.find() queries to complete
+                const optimizerPromises = await Promise.all(allLocation.map(async (location) => {
+                    const allGateway = await GatewayModel.find({ EnterpriseInfo: location._id });
+
+                    const gatewayPromises = allGateway.map(async (gateway) => {
+                        const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                        return allOptimizer.map((item) => item._id);
+                    });
+
+                    return Promise.all(gatewayPromises);
+                }));
+
+                return optimizerPromises.flat();
+            }));
+            // Flatten the array of arrays containing optimizer IDS
+            const flattenedOptimizerIDs = optimizerIDS.flat();
+            // console.log(flattenedOptimizerIDs.flat());
+            result = await UpdateSettings(flattenedOptimizerIDs.flat(), data);
+        }
 
         if (result) {
-            return res.send({ success: true, message: `Settings values set successfully.` });
+            return res.send({ success: true, message: `Optimizer Settings values Set successfully.` });
         } else {
             return res.status(500).send({ success: false, message: 'Internal Server Error: Unable to set result.' });
         }
@@ -252,77 +339,133 @@ exports.SetOptimizerSettingValue = async (req, res) => {
     }
 };
 
-
-
+// ResetOptimizerSettingValue
 exports.ResetOptimizerSettingValue = async (req, res) => {
+    var result = "";
+    var data = "";
+    try {
+        // reset particular optimizer
+        if (req.body.group === 'optimizer') {
+            console.log(`Resetting to default value particular optimizer ${req.body.id}`);
+            const optimizerIDS = [req.body.id]
+            result = await UpdateSettings(optimizerIDS, data);
+        }
 
+        // reset all optimizer assign with the gateway => optimizers
+        if (req.body.group == 'gateway') {
+            console.log(`Resetting to default value gateway => optimizers`);
+            const gateway_id = req.body.id;
+            const allOptimizer = await OptimizerModel.find({ GatewayId: gateway_id });
+            const optimizerIDS = await Promise.all(allOptimizer.map(async (item) => {
+                return item._id;
+            }));
+
+            result = await UpdateSettings(optimizerIDS, data);
+        }
+
+        // reset all optimizer assign with the location => gateways => optimizers
+        if (req.body.group == 'location') {
+            console.log(`Resetting to default value location => gateways => optimizers`);
+            const location_id = req.body.id;
+            const Location = await LocationModel.findOne({ _id: location_id });
+            const allGateway = await GatewayModel.find({ EnterpriseInfo: Location._id });
+
+            const optimizerIDS = await Promise.all(allGateway.map(async (gateway) => {
+                const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                return allOptimizer.map((item) => item._id);
+            }));
+
+            result = await UpdateSettings(optimizerIDS.flat(), data);
+        }
+
+        // reset all optimizer assign with the state => locations => gateways => optimizers
+        if (req.body.group == 'state') {
+            console.log(`Resetting to default value state => locations => gateways => optimizers`);
+            const state_id = req.body.id;
+            const State = await StateModel.findOne({ _id: state_id });
+            const allLocation = await LocationModel.find({ State_ID: State.State_ID });
+            const optimizerIDS = await Promise.all(allLocation.map(async (location) => {
+                const allGateway = await GatewayModel.find({ EnterpriseInfo: location._id });
+
+                // Using Promise.all to wait for all OptimizerModel.find() queries to complete
+                const optimizerPromises = allGateway.map(async (gateway) => {
+                    const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                    return allOptimizer.map((item) => item._id);
+                });
+
+                return Promise.all(optimizerPromises);
+            }));
+            const flattenedOptimizerIDs = optimizerIDS.flat();
+            // console.log(flattenedOptimizerIDs.flat());
+            result = await UpdateSettings(flattenedOptimizerIDs.flat(), data);
+        }
+
+        // reset all optimizer assign with the enterprise => states => locations => gateways => optimizers
+        if (req.body.group === 'enterprise') {
+            console.log(`Resetting to default value enterprise => states => locations => gateways => optimizers`);
+
+            const enterprise_id = req.body.id;
+            const Enterprise = await EnterpriseModel.findOne({ _id: enterprise_id });
+            const allState = await StateModel.find({ Enterprise_ID: Enterprise._id });
+
+            const optimizerIDS = await Promise.all(allState.map(async (state) => {
+                const allLocation = await LocationModel.find({ State_ID: state.State_ID });
+
+                // Using Promise.all to wait for all OptimizerModel.find() queries to complete
+                const optimizerPromises = await Promise.all(allLocation.map(async (location) => {
+                    const allGateway = await GatewayModel.find({ EnterpriseInfo: location._id });
+
+                    const gatewayPromises = allGateway.map(async (gateway) => {
+                        const allOptimizer = await OptimizerModel.find({ GatewayId: gateway._id });
+                        return allOptimizer.map((item) => item._id);
+                    });
+
+                    return Promise.all(gatewayPromises);
+                }));
+
+                return optimizerPromises.flat();
+            }));
+            // Flatten the array of arrays containing optimizer IDS
+            const flattenedOptimizerIDs = optimizerIDS.flat();
+            // console.log(flattenedOptimizerIDs.flat());
+            result = await UpdateSettings(flattenedOptimizerIDs.flat(), data);
+        }
+
+        if (result) {
+            return res.send({ success: true, message: `Optimizer Settings values Reset successfully.` });
+        } else {
+            return res.status(500).send({ success: false, message: 'Internal Server Error: Unable to set result.' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ success: false, message: `Internal Server Error: ${error.message}` });
+    }
+}
+
+
+// Common UpdateSettings functions for both set and reset
+const UpdateSettings = async (optimizerIDS, data) => {
     // first get the optimizer default value
     const defaultValue = await OptimizerDefaultSettingValueModel.findOne();
     let resetValues = {
-        powerOnObservation: defaultValue.powerOnObservation,
-        maxCompressorTurnoffCountPerHour: defaultValue.maxCompressorTurnoffCountPerHour,
-        optimizationTime: defaultValue.optimizationTime,
-        steadyStateRoomTemperatureTolerance: defaultValue.steadyStateRoomTemperatureTolerance,
-        steadyStateCoilTemperatureTolerance: defaultValue.steadyStateCoilTemperatureTolerance,
-        steadyStateSamplingDuration: defaultValue.steadyStateSamplingDuration,
-        minAirConditionerOffDuration: defaultValue.minAirConditionerOffDuration,
-        airConditionerOffDeclarationMinPeriod: defaultValue.airConditionerOffDeclarationMinPeriod,
-        maxObservationTime: defaultValue.maxObservationTime,
-        thermoStateTimeIncrease: defaultValue.thermoStateTimeIncrease,
-        thermoStateInterval: defaultValue.thermoStateInterval
+        powerOnObservation: data ? data.powerOnObservation : defaultValue.powerOnObservation,
+        maxCompressorTurnoffCountPerHour: data ? data.maxCompressorTurnoffCountPerHour : defaultValue.maxCompressorTurnoffCountPerHour,
+        optimizationTime: data ? data.optimizationTime : defaultValue.optimizationTime,
+        steadyStateRoomTemperatureTolerance: data ? data.steadyStateRoomTemperatureTolerance : defaultValue.steadyStateRoomTemperatureTolerance,
+        steadyStateCoilTemperatureTolerance: data ? data.steadyStateCoilTemperatureTolerance : defaultValue.steadyStateCoilTemperatureTolerance,
+        steadyStateSamplingDuration: data ? data.steadyStateSamplingDuration : defaultValue.steadyStateSamplingDuration,
+        minAirConditionerOffDuration: data ? data.minAirConditionerOffDuration : defaultValue.minAirConditionerOffDuration,
+        airConditionerOffDeclarationMinPeriod: data ? data.airConditionerOffDeclarationMinPeriod : defaultValue.airConditionerOffDeclarationMinPeriod,
+        maxObservationTime: data ? data.maxObservationTime : defaultValue.maxObservationTime,
+        thermoStateTimeIncrease: data ? data.thermoStateTimeIncrease : defaultValue.thermoStateTimeIncrease,
+        thermoStateInterval: data ? data.thermoStateInterval : defaultValue.thermoStateInterval
     };
-    // reset particular optimizer
-    if (req.body.group === 'optimizer') {
-        console.log(`Resetting to default value particular optimizer ${req.body.id}`);
-        // Now, you can proceed with your findOneAndUpdate logic
-        resetValues.optimizerID = req.body.id;
-        const result = await OptimizerSettingValueModel.findOneAndUpdate(
-            { optimizerID: req.body.id },
+    return await Promise.all(optimizerIDS.map(async id => {
+        resetValues.optimizerID = id.toString();
+        return await OptimizerSettingValueModel.findOneAndUpdate(
+            { optimizerID: id },
             { $set: resetValues },
             { new: true, upsert: true }
         );
-
-        if (result) {
-            return res.send({ success: true, message: `Settings values set successfully.` });
-        } else {
-            return res.status(500).send({ success: false, message: 'Internal Server Error: Unable to get result.' });
-        }
-    }
-
-
-    // reset all optimizer assign with the gateway => optimizers
-    if (req.body.group == 'gateway') {
-        console.log(`Resetting to default value gateway => optimizers`);
-    }
-    // reset all optimizer assign with the location => gateways => optimizers
-    if (req.body.group == 'location') {
-        console.log(`Resetting to default value location => gateways => optimizers`);
-    }
-    // reset all optimizer assign with the state => locations => gateways => optimizers
-    if (req.body.group == 'state') {
-        console.log(`Resetting to default value state => locations => gateways => optimizers`);
-    }
-    // reset all optimizer assign with the enterprise => states => locations => gateways => optimizers
-    if (req.body.group == 'enterprise') {
-        console.log(`Resetting to default value enterprise => states => locations => gateways => optimizers`);
-    }
-
-    // return;
-
-
-    return res.send(resetValues);
-
-
-    try {
-
-        const defaultValue = new OptimizerSettingValueModel(resetValues);
-        await defaultValue.save();
-        res.send({ success: true, message: 'Record inserted.' });
-
-
-    } catch (error) {
-        // Handle any errors that may occur during the process
-        console.error(error);
-        res.status(500).send({ success: false, message: 'Internal Server Error' });
-    }
+    }));
 }

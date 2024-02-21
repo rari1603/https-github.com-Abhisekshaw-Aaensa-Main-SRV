7930654e4d7d0f44e3bd575975d2d213706e677b
Satyajit <satyajit@kotaielectronics.com>
2024-02-22 00:26:08 +0530

    register open

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const AuthController = require('../controllers/auth.controller');
 const { HandleLoginError, ForgetPasswordValidation } = require('../middleware/auth.validation');
 
 const router = express.Router();
-// router.post('/register', AuthController.register); // Closed
+router.post('/register', AuthController.register); // Closed
 
 router.post('/login', [HandleLoginError], AuthController.login);
 router.post('/forget-password', [ForgetPasswordValidation], AuthController.ForgetPassword);

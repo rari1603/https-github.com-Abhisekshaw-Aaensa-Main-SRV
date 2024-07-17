be3b20da1ed3c322cebd885199e25f6c9212ccec
Abhisekshaw <abhishek.shaw@aaensa.com>
2024-07-17 17:49:47 +0530

    log 6

--- controllers/SuAdmin/report.controller.js
+++ controllers/SuAdmin/report.controller.js
@@ -168,8 +168,8 @@ exports.AllDeviceData = async (req, res) => {
             .limit(validatedPageSize)
             .lean();
 
-            console.log(OptimizerLogs);
-            return res.send(OptimizerLogs);
+            // console.log(OptimizerLogs);
+            // return res.send(OptimizerLogs);
 
         const gatewayIdToLogs = OptimizerLogs.reduce((acc, log) => {
             if (!acc[log.GatewayID]) acc[log.GatewayID] = [];

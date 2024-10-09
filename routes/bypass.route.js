23c2812361eeb47512c770d2332277ad98146250
MadhuKannaujiya5 <madhu@aaensa.com>
2024-10-09 12:28:27 +0530

    enterprise id updated with api url

--- routes/bypass.route.js
+++ routes/bypass.route.js
@@ -3,5 +3,5 @@ const BypassController = require('../controllers/Bypass/BypassController');
 const router = express.Router();
 
 // Store bypass data.
-router.get('/enterprise/bypass/data', BypassController.EnterpriseBypassStatus);
+router.get('/enterprise/bypass/data/:enterprise_id', BypassController.EnterpriseBypassStatus);
 module.exports = router;
